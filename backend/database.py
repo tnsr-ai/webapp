d06bf5ac1ae0a8be972124aa79638564828133db
Amit Bera <amitalokbera@gmail.com>
2023-12-16 19:18:33 +0530

    fixed celery implementation for email task in options

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:beraamit@localhost/tnsr"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:amitbera@localhost/development"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
