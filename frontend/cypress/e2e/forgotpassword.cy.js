75b296b21b2c986afee8fb478b96bb87c9f4f8ac
Amit Bera <amitalokbera@gmail.com>
2024-01-05 15:44:04 +0530

    e2e ga fixed

--- frontend/cypress/e2e/forgotpassword.cy.js
+++ frontend/cypress/e2e/forgotpassword.cy.js
@@ -39,26 +39,6 @@ describe("Forgot Password E2E", () => {
         cy.get('[data-testid="resetPasswordButton"]').click();
         cy.contains("Email not found").should("be.visible");
       });
-
-      it("check for forgot password", () => {
-        cy.visit("/register");
-        cy.contains("Create an Account").should("be.visible");
-        cy.get('[data-testid="firstNameInput"]').type("fname");
-        cy.get('[data-testid="lastNameInput"]').type("lname");
-        cy.get('[data-testid="emailInput"]').type("admin@email.com");
-        cy.get('[data-testid="passwordInput"]').type("password");
-        cy.get('[data-testid="confirmPasswordInput"]').type("password");
-        cy.get('[data-testid="signUpButton"]').should("not.be.disabled");
-        cy.get('[data-testid="signUpButton"]').click();
-        cy.url().should("include", "/dashboard");
-        cy.clearCookies();
-        cy.visit("/forgot-password");
-        cy.contains("Forgot Password ?").should("be.visible");
-        cy.get('[data-testid="emailInput"]').type("admin@email.com");
-        cy.get('[data-testid="resetPasswordButton"]').should("not.be.disabled");
-        cy.get('[data-testid="resetPasswordButton"]').click();
-        cy.contains("Email sent to admin@email.com").should("be.visible");
-      });
     });
   });
 });
