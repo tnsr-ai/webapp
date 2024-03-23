054b6bfc11aaffdeffbcc28efcc917e6a220e795
Amit Bera <amitalokbera@gmail.com>
2024-03-23 09:01:57 +0530

    fixed set cookies depending on the app_env

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -38,6 +38,7 @@ from utils import (
     REDIS_HOST,
     REDIS_PORT,
     TNSR_DOMAIN,
+    APP_ENV,
     create_access_token,
     create_refresh_token,
     forgotpassword_email,
@@ -138,7 +139,6 @@ def minutes_to_delta(minutes: int):
 
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login", auto_error=False)
-
 
 def get_current_user(db: db_dependency, token: str = Depends(oauth2_bearer)):
     try:
@@ -387,26 +387,44 @@ async def create_user(
             "token_type": "bearer",
         }
     )
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
-    response.set_cookie(
-        key="refreshToken",
-        value=refreshToken,
-        max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
+    if APP_ENV == "production":
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+    else:
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
     logger.info(f"User created - {hide_email(create_user_request.email)}")
     verification_link = f"{TNSR_DOMAIN}/verifyemail/?user_id={get_user.id}&email_token={result['data']['email_token']}"
     send_email_task.delay(
@@ -483,26 +501,44 @@ async def login_user(
             "token_type": "bearer",
         }
     )
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
-    response.set_cookie(
-        key="refreshToken",
-        value=refreshToken,
-        max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
+    if APP_ENV == "production":
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+    else:
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
     logger.info(f"User logged in - {hide_email(form_data.username)}")
     return {
         "data": content,
@@ -670,26 +706,44 @@ async def check_user_refresh(
         "refreshToken": refreshToken,
         "token_type": "bearer",
     }
-    response.set_cookie(
-        key="access_token",
-        value=accessToken,
-        max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None",
-    )
-    response.set_cookie(
-        key="refreshToken",
-        value=refreshToken,
-        max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
+    if APP_ENV == "production":
+        response.set_cookie(
+            key="access_token",
+            value=accessToken,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+    else:
+        response.set_cookie(
+            key="access_token",
+            value=accessToken,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
     logger.info(f"User refreshed - {current_user.user_id}")
     return {"data": content, "detail": "Success"}
 
@@ -807,26 +861,44 @@ async def google_callback(
         "lastname": user.last_name,
         "email": user.email,
     }
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
-    response.set_cookie(
-        key="refreshToken",
-        value=refreshToken,
-        max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
-        path="/",
-        domain=".tnsr.ai",
-        secure=True,
-        httponly=False,
-        samesite="None"
-    )
+    if APP_ENV == "production":
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            domain=".tnsr.ai",
+            secure=True,
+            httponly=False,
+            samesite="None"
+        )
+    else:
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age=minutes_to_delta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
+        response.set_cookie(
+            key="refreshToken",
+            value=refreshToken,
+            max_age=minutes_to_delta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+            path="/",
+            secure=False,
+            samesite="lax",
+        )
     content.update(
         {
             "refreshToken": refreshToken,
